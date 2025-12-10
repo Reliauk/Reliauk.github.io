fec6187182b9dce79c327fabd175e7e3452f70e6
Reliauk <reliauk@foxmail.com>
2025-12-11 01:57:35 +0800

    a

--- QOJ/15108/main.cpp
+++ QOJ/15108/main.cpp
@@ -12,14 +12,70 @@ const int N = 3E5 + 5;
 
 int n, m;
 std::vector<std::pair<int, i64>> ch;
-std::array<i64, N> a, c, sa, sc, val, vmo, ans;
+std::array<i64, N> a, c, sa, sc, ans;
+
+struct I {
+  int l, r;
+  i64 s0, s1, v;
+  bool c;
+};
+std::array<I, N * 4> t;
+#define LC (o + o)
+#define RC (o + o + 1)
+void Up(int o) {
+  t[o].s0 = t[LC].s0 + t[RC].s0;
+  t[o].s1 = t[LC].s1 + t[RC].s1;
+}
+void Upd(int o, i64 v) {
+  t[o].v += v, t[o].s0 += (t[o].r - t[o].l + 1) * v;
+  t[o].s1 += (sc[t[o].r] - sc[t[o].l - 1]) * v;
+}
+void Cov(int o) { t[o].s0 = t[o].s1 = t[o].v = 0, t[o].c = true; }
+void Push(int o) {
+  if (t[o].c) Cov(LC), Cov(RC), t[o].c = false;
+  if (t[o].v) Upd(LC, t[o].v), Upd(RC, t[o].v), t[o].v = 0;
+}
+void Make(int o, int l, int r) {
+  t[o].l = l, t[o].r = r;
+  if (l == r) return;
+  int m = (l + r) >> 1;
+  Make(LC, l, m), Make(RC, m + 1, r);
+}
+void Add(int o, int x, int y, i64 v) {
+  int l = t[o].l, r = t[o].r;
+  if (x <= l && r <= y) return Upd(o, v);
+  if (x > r || l > y) return;
+  Push(o), Add(LC, x, y, v), Add(RC, x, y, v), Up(o);
+}
+void Reset(int o, int x, int y) {
+  int l = t[o].l, r = t[o].r;
+  if (x <= l && r <= y) return Cov(o);
+  if (x > r || l > y) return;
+  Push(o), Reset(LC, x, y), Reset(RC, x, y), Up(o);
+}
+i64 Sum(int o, int x, int y) {
+  int l = t[o].l, r = t[o].r;
+  if (x <= l && r <= y) return t[o].s1;
+  if (x > r || l > y) return 0;
+  return Push(o), Sum(LC, x, y) + Sum(RC, x, y);
+}
+#undef LC
+#undef RC
+
+std::map<int, i64> vmo;
+std::array<int, N> f;
+std::array<i64, N> g;
 
 void Modify(int p, int it) {
-  i64 sum = sc[it] - a[p + 1] + val[it] * c[it] + vmo[it];
-  for (int i = 1; i < it; ++i)
-    sum += val[i] * c[i] + vmo[i], val[i] = vmo[i] = 0;
-  if (it <= m) val[it] = sum / c[it], vmo[it] = sum % c[it];
-  for (int i = it + 1; i <= m; ++i) ++val[i];
+  i64 sum = sc[it] - a[p + 1] + Sum(1, 1, std::min(it, m));
+  Reset(1, 1, std::min(it, m));
+  while (vmo.size() && begin(vmo)->first <= it)
+    sum += begin(vmo)->second, vmo.erase(begin(vmo));
+  if (it <= m) {
+    Add(1, it, it, sum / c[it]);
+    if (sum % c[it]) vmo[it] = sum % c[it];
+  }
+  if (it < m) Add(1, it + 1, m, 1);
 }
 
 int main() {
@@ -30,7 +86,7 @@ int main() {
   for (int i = 1; i <= n; ++i) std::cin >> a[i], sa[i] = sa[i - 1] + a[i];
   for (int i = 1; i <= m; ++i) std::cin >> c[i], sc[i] = sc[i - 1] + c[i];
 
-  ch = {{n, sa[n]}};
+  ch = {{n, sa[n]}}, Make(1, 1, m);
   for (int p = n - 1; ~p; --p) {
     for (; ch.size() >= 2; ch.pop_back()) {
       auto [a, b] = ch.back();
@@ -40,9 +96,8 @@ int main() {
     i64 slop = (ch.back().second - sa[p]) / (ch.back().first - p);
     Modify(p, std::upper_bound(PAR(sc, 1, m + 1), slop) - data(sc));
     ch.emplace_back(p, sa[p]);
-    i64 ans = 0, r = 0;
-    for (int i = 1; i <= m; ++i)
-      ans += (vmo[i] > r) + val[i], r += (vmo[i] > r) * c[i] - vmo[i];
+    i64 ans = t[1].s0, r = 0;
+    for (auto [x, v] : vmo) ans += v > r, r += (v > r) * c[x] - v;
     ::ans[p] = i64(n - p) * m - ans;
   }
 
