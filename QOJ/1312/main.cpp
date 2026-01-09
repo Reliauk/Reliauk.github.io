75ff7e3d748e73c6f86033d8901af365831ff286
Reliauk <reliauk@foxmail.com>
2026-01-09 22:42:28 +0800

    a

--- QOJ/1312/main.cpp
+++ QOJ/1312/main.cpp
@@ -12,8 +12,22 @@ int n, m;
 std::array<int, N> a, b;
 std::array<std::vector<std::pair<int, int>>, N> adj;
 
+int sa, sb;
 std::vector<int> cur;
+std::pair<int, int> ext;
 std::array<bool, N> vis;
+void Dfs(int u, int fa) {
+  vis[u] = true, cur.push_back(u);
+  for (auto [v, w] : adj[u])
+    if (!vis[v]) {
+      Dfs(v, u);
+      if (u < v)
+        sb += b[w];
+      else
+        sa += a[w];
+    } else if (v - fa)
+      ext = {v, u};
+}
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -28,6 +42,10 @@ int main() {
 
   for (int i = 1; i <= n + n; ++i) {
     if (vis[i]) continue;
+    cur.clear(), ext = {0, 0}, sa = sb = 0, Dfs(i, 0);
+    if (ext.first) {
+    } else {
+    }
   }
 
   return 0;
