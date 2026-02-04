3cdb18fcf24fadfcfca96ce81c883c8f42e88b73
Reliauk <reliauk@foxmail.com>
2026-02-04 11:14:18 +0800

    a

--- QOJ/5446/main.cpp
+++ QOJ/5446/main.cpp
@@ -6,43 +6,83 @@ using u32 = uint32_t;
 using i64 = int64_t;
 using u64 = uint64_t;
 
-const int N = 205;
+const int N = 205, S = 1E3 + 5, INF = 1E9;
+namespace flow {
+struct E {
+  int v, w, i;
+  E(int v_, int w_, int i_) { v = v_, w = w_, i = i_; }
+};
+int n;
+std::array<std::vector<E>, S> adj;
+void Edge(int u, int v, int w) {
+  adj[u].emplace_back(v, w, adj[v].size());
+  adj[v].emplace_back(u, 0, adj[u].size() - 1);
+}
+std::array<int, S> arc, lev;
+void Reset() {
+  for (int i = 0; i <= n; ++i) adj[i].clear();
+  n = 0;
+}
+int Flow(int s, int t) {
+  auto bfs = [&]() {
+    for (int i = 0; i <= n; ++i) arc[i] = 0, lev[i] = -1;
+    std::queue<int> q;
+    lev[s] = 0, q.emplace(s);
+    for (int u; q.size();) {
+      u = q.front(), q.pop();
+      for (auto [v, w, _] : adj[u])
+        if (w && lev[v] < 0) lev[v] = lev[u] + 1, q.emplace(v);
+    }
+    return ~lev[t];
+  };
+  auto dinic = [&](auto self, int u, int f) {
+    if (u == t || !f) return f;
+    int ret = 0;
+    for (int &i = arc[u]; i < adj[u].size(); ++i) {
+      auto [v, w, j] = adj[u][i];
+      if (!w || lev[v] - lev[u] - 1) continue;
+      int r = self(self, v, std::min(f, w));
+      f -= r, ret += r, adj[u][i].w -= r, adj[v][j].w += r;
+      if (!f) break;
+    }
+    return ret;
+  };
+  int flow = 0;
+  while (bfs()) flow += dinic(dinic, s, INF);
+  return flow;
+}
+}  // namespace flow
 
 int n;
-std::array<std::array<int, N>, N> a, pos;
-std::vector<std::tuple<int, int, int, int>> ans;
-
-void Print() {
-  std::cout << ans.size() << '\n';
-  for (auto [a, b, c, d] : ans)
-    std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
-}
+std::array<std::array<int, N>, N> a, p, pos;
 
 void Proc() {
-  std::cin >> n, ans.clear();
+  std::cin >> n;
   for (int i = 1; i <= n; ++i)
-    for (int j = 1; j <= n; ++j) std::cin >> a[i][j], pos[i][j] = j;
+    for (int j = 1; j <= n; ++j) std::cin >> a[i][j], pos[i][j] = 0;
 
-  if (n == 2) {
-    if (a[1][1] == a[1][2]) ans.emplace_back(1, 1, 2, 1);
-    return Print();
+  for (int c = 1; c <= n; ++c) {
+    flow::Reset(), flow::n = n + n + 1;
+    for (int i = 1; i <= n; ++i)
+      flow::Edge(0, i, 1), flow::Edge(i + n, flow::n, 1);
+    static std::array<std::map<int, int>, N> rep;
+    for (int i = 1; i <= n; ++i) {
+      rep[i].clear();
+      for (int j = 1; j <= n; ++j)
+        if (!pos[i][j] && !rep[i].count(a[i][j]))
+          flow::Edge(a[i][j], i + n, 1), rep[i][a[i][j]] = j;
+    }
+    assert(flow::Flow(0, flow::n) == n);
+    for (int i = 1; i <= n; ++i)
+      for (auto [v, w, j] : flow::adj[i])
+        if (v > n && !w)
+          p[v - n][pos[v - n][rep[v - n][i]] = c] = rep[v - n][i];
   }
 
-  for (int i = 1; i <= n; ++i) {
-    std::map<int, int> cnt;
-    for (int j = 1; j <= n; ++j) ++cnt[a[i][j]];
-    int sp = cnt.size() < 2           ? begin(cnt)->first
-             : begin(cnt)->second > 1 ? rbegin(cnt)->first
-                                      : begin(cnt)->first;
-    for (int j = 1; j < n; ++j)
-      if (a[i][j] == sp)
-        std::swap(a[i][j], a[i][j + 1]), std::swap(pos[i][j], pos[i][j + 1]);
-  }
-
+  std::cout << n * (n - 1) / 2 << '\n';
   for (int i = 1; i <= n; ++i)
-    for (int j = 1; j < i; ++j) ans.emplace_back(i, pos[i][j], j, pos[j][i]);
-
-  Print();
+    for (int j = 1; j < i; ++j)
+      std::cout << i << ' ' << p[i][j] << ' ' << j << ' ' << p[j][i] << '\n';
 }
 
 int main() {
--- QOJ/5450/main.cpp
+++ QOJ/5450/main.cpp
@@ -6,62 +6,64 @@ using u32 = uint32_t;
 using i64 = int64_t;
 using u64 = uint64_t;
 
-const int N = 405;
+const int N = 2E3 + 5;
 std::mt19937_64 rng(20090627);
 
 int n, m;
-std::array<std::vector<int>, N> club;
-std::array<std::array<std::array<bool, N>, N>, N> vis;
+std::array<std::array<int, N>, N> c;
+int S(int u, int v, int w) { return c[u][v] == c[v][w] && c[u][v] >= 0; }
 
 void Proc() {
   std::cin >> n >> m;
-  for (int i = 1; i <= n; ++i) club[i].clear();
-  bool no = false;
+  for (int i = 1; i <= n; ++i)
+    for (int j = 1; j <= n; ++j) c[i][j] = -1;
+  std::vector<int> club;
   for (int i = 0, k; i < m; ++i) {
     std::cin >> k;
-    for (int j = 0, x; j < k; ++j) std::cin >> x, club[x].push_back(i);
-    if ((k + 1) / 2 > (n - k)) no = true;
-  }
-  if (no) return void(std::cout << "-1\n");
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= n; ++j) {
-      std::vector<int> cnt(m);
-      for (int x : club[i]) ++cnt[x];
-      for (int x : club[j]) ++cnt[x];
-      for (int k = 1; k <= n; ++k) {
-        bool suc = false;
-        for (int x : club[k]) {
-          suc |= cnt[x] == 2;
-          if (suc) break;
-        }
-        vis[i][j][k] = !suc;
-      }
-    }
+    std::vector<int> v(k);
+    for (int &j : v) std::cin >> j;
+    if (k > club.size()) club = v;
+    for (int p : v)
+      for (int q : v) c[p][q] = i;
   }
 
+  if (n - club.size() < (club.size() + 1) / 2) return void(std::cout << "-1\n");
+
   std::vector<int> perm(n);
-  std::iota(ALL(perm), 1), std::shuffle(ALL(perm), rng);
-  int ok = 0;
-  auto val = [&](int i) -> int {
-    i %= n;
-    int j = (i + n - 2) % n, k = (i + n - 1) % n;
-    return vis[perm[i]][perm[j]][perm[k]];
+  static std::array<bool, N> in;
+  for (int i = 1; i <= n; ++i) in[i] = false;
+  for (int v : club) in[v] = true;
+  for (int i = 0, p = 0, q = 1; i < n; ++i)
+    if (i % 3 == 2 || p >= club.size()) {
+      while (in[q]) ++q;
+      perm[i] = q++;
+    } else
+      perm[i] = club[p++];
+
+  auto calc = [&](int i) {
+    return S(perm[i], perm[(i + 1) % n], perm[(i + 2) % n]);
   };
-  for (int i = 0; i < n; ++i) ok += val(i);
-  while (ok - n) {
-    int i = (rng() % (n - 1) + n - 1) % (n - 1);
-    int nok = ok;
-    nok -= val(i) + val(i + 1) + val(i + 2) + val(i + 3);
-    std::swap(perm[i], perm[i + 1]);
-    nok += val(i) + val(i + 1) + val(i + 2) + val(i + 3);
-    if (nok > ok || (rng() & 1)) {
-      ok = nok;
-      continue;
-    }
-    std::swap(perm[i], perm[i + 1]);
+  int val = 0;
+  for (int i = 0; i < n; ++i) val += calc(i);
+  while (val) {
+    int x = rng() % n, y = rng() % n, nv = val;
+    std::set<int> ind{x,
+                      y,
+                      (x + n - 1) % n,
+                      (x + n - 2) % n,
+                      (y + n - 1) % n,
+                      (y + n - 2) % n};
+    for (int i : ind) nv -= calc(i);
+    std::swap(perm[x], perm[y]);
+    for (int i : ind) nv += calc(i);
+    if (nv < val || (nv == val) && (rng() % 2))
+      val = nv;
+    else
+      std::swap(perm[x], perm[y]);
   }
+
   for (int i : perm) std::cout << i << ' ';
-  return void(std::cout << '\n');
+  std::cout << '\n';
 }
 
 int main() {
